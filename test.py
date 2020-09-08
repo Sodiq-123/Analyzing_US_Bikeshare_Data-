8644c79faf2d1ece9204e11541dc521cd3797570
OdenDavid <davidodenikpi@gmail.com>
2020-09-08 16:31:50 +0100

    First Commit

---
+++ test.py
@@ -0,0 +1,10 @@
+CITY_DATA = { 'chicago': 'chicago.csv',
+              'new york city': 'new_york_city.csv',
+              'washington': 'washington.csv' }
+
+months = ['january','february','march','april','may','june']
+days = ['sun','mon','tue','wed','thu','fri','sat']
+
+num = 1999.0
+int_num = int(num)
+print(int_num)
