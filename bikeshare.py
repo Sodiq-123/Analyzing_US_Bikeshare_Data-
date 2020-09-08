8167f6a88b53d26e0741c9b8235c808be70c4740
OdenDavid <davidodenikpi@gmail.com>
2020-09-08 20:25:29 +0100

    filter function updated

--- bikeshare.py
+++ bikeshare.py
@@ -5,10 +5,17 @@ import numpy as np
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
-month = ''
-day = ''
-months = ['january','february','march','april','may','june']
-days = [str(i) for i in range(1,32)]
+
+month = '' # variable to hold the value for month
+day = '' # variable to hold the value for day
+months = ['january','february','march','april','may','june'] # list of months
+days = []
+for i in range(1,32):
+    if i <= 9:
+        days.append('0'+str(i))
+    else:
+        days.append(str(i))
+        
 def get_filters():
     print('Hello! Let\'s explore some US bikeshare data!')
     while True:
@@ -27,7 +34,7 @@ def get_filters():
                     break #break the loop
                 elif choice.lower() == 'day': # if the filter choice by the user is DAY
                     while True:
-                        day = input("Enter Day(Date): 1,2,3,....,31\n") # Get the day to filter from user
+                        day = input("Enter Day(Date): 01,02,03,....,31\n") # Get the day to filter from user
                         if day in days: # if day entered by user is in the days list break the loop
                             break
                         else: # else continue looping until the right input is entered
@@ -36,11 +43,10 @@ def get_filters():
                     break #break the loop
                 elif choice.lower() == 'both': # if the filter choice by the user is BOTH
                     while True:
-         
-                        both = input("Enter Month (space) day(date): January 5\n")
-                        month = both.split(' ')[0] # assign the month variable to first value in both
-                        day = both.split(' ')[1] # assign the day variable to second value in both
-                        if month in months and day in days:
+                        both = input("Enter Month (space) day(date): January 05\n")
+                        if both.split(' ')[0] in months and both.split(' ')[1] in days:
+                            month = both.split(' ')[0] # assign the month variable to first value in both
+                            day = both.split(' ')[1] # assign the day variable to second value in both                             
                             break
                         else:
                             print("Invalid month or day, try again!")
@@ -59,15 +65,23 @@ def get_filters():
     return city, month, day
 
 def load_data(city, month, day):
-    
+    days = []
+    for i in range(1,32):
+        if i <= 9:
+            days.append('0'+str(i))
+        else:
+            days.append(str(i))
+    months = ['january','february','march','april','may','june'] # list of months
+    months_dict = dict(zip(months, days))    
     """Chicago"""
     if city.lower() == 'chicago': # if the city chosen by the user is chicago
         df = pd.read_csv(CITY_DATA.get('chicago')) # read the chicago data set
         df['Gender'].replace(np.nan,'Male',inplace=True) # replace the missing values in the gender column using mode since its a categorically represented
         df['Birth Year'].replace(np.nan,df['Birth Year'].mean(),inplace=True) # replace the missing values in the birth year column using mean since it contains contionous values 
-        """Month Filter"""
         if month != '' and day == '': # if month is not empty and day is empty means we are filtering by MONTH
-            df = df.loc[df['Start Time'].str[5:7]=='04']
+            df = df.loc[df['Start Time'].str[5:7] == months_dict.get(month)]
+        elif month == '' and day != '': # if month is empty and day is not empty means we are filtering by DAY
+            df = df.loc[df['Start Time'].str[8:10] == ]            
         
     return df
 def main():
--- test.py
+++ test.py
@@ -8,3 +8,11 @@ days = ['sun','mon','tue','wed','thu','fri','sat']
 num = 1999.0
 int_num = int(num)
 print(int_num)
+
+days = []
+for i in range(1,32):
+    if i <= 9:
+        days.append('0'+str(i))
+    else:
+        days.append(str(i))
+print(days)
